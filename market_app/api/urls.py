3f1729360561446e8f816b0242d4a37142b37dc4
Magdalena260 <magdalena.laurisch@outlook.de>
2026-09-21 18:44:11 +0200

    Fix API urls

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
+from django.urls import path
 from .views import markets_view, market_single_view
 
+
 urlpatterns = [
-    
-    path('', markets_view)
-    path('<int:pk>/', markets_single_view),
+    path('', markets_view),
+    path('<int:pk>/', market_single_view),
 ]
