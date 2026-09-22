e91e5bf588be534aa5e9b97c5d1750bcfdccae55
Magdalena260 <magdalena.laurisch@outlook.de>
2026-09-22 14:26:50 +0200

    Add seller serializers and update API

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,18 +1,78 @@
 from rest_framework import serializers
-from _market_app.models import Market
+from market_app.models import Market, Seller
+
+
+def validate_no_x(value):
+    errors = []
+
+    if 'X' in value:
+        errors.append('no X in location')
+
+    if 'Y' in value:
+        errors.append('no Y in location')
+
+    if errors:
+        raise serializers.ValidationError(errors)
+
+    return value
 
 
 class MarketSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    content = serializers.CharField(max_length=200)
-    created = serializers.DateTimeField()
-
-    class Market(serializers.Serializer):
-        id = serializers.IntegerField(read_only=True)
-        name = serializers.CharField(max_length=255)
-        location = serializers.CharField(max_length=255)
-        description =serializers.TextField()
-        net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    location = serializers.CharField(
+        max_length=255,
+        validators=[validate_no_x]
+    )
+    description = serializers.CharField()
+    net_worth = serializers.DecimalField(
+        max_digits=100,
+        decimal_places=2
+    )
 
     def create(self, validated_data):
-        return Market().objects.create(**validated_data)
+        return Market.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.location = validated_data.get(
+            'location',
+            instance.location
+        )
+        instance.description = validated_data.get(
+            'description',
+            instance.description
+        )
+        instance.net_worth = validated_data.get(
+            'net_worth',
+            instance.net_worth
+        )
+        instance.save()
+        return instance
+
+    def validate_location(self, value):
+        if 'X' in value:
+            raise serializers.ValidationError('no X in location')
+        return value
+
+
+class SellerDetailSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    contact = serializers.CharField()
+    market = MarketSerializer(many=True, read_only=True)
+
+
+class SellerCreateSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    contact = serializers.CharField()
+
+    def create(self, validated_data):
+        return Seller.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.contact = validated_data.get('contact', instance.contact)
+        instance.save()
+        return instance
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,38 +1,73 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import MarketSerializer
-from market_app.models import Market
+from .serializers import (
+    MarketSerializer,
+    SellerDetailSerializer,
+    SellerCreateSerializer,
+)
+from market_app.models import Market, Seller
 
 
 @api_view(['GET', 'POST'])
 def markets_view(request):
 
-       if request.method == 'GET':
+    if request.method == 'GET':
         markets = Market.objects.all()
         serializer = MarketSerializer(markets, many=True)
         return Response(serializer.data)
 
-        if request.method == 'POST':
-            serializer = MarketSerializer(data=request.data)
+    if request.method == 'POST':
+        serializer = MarketSerializer(data=request.data, partial=True)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
-          return Response(serializer.errors)
+            return Response(serializer.errors)
 
 
-@api_view(['GET', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def market_single_view(request, pk):
 
-   if request.method == 'GET':
+    if request.method == 'GET':
         market = Market.objects.get(pk=pk)
         serializer = MarketSerializer(market)
         return Response(serializer.data)
 
+    if request.method == 'PUT':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market, data=request.data)
 
-   if request.method == 'DELETE':
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'DELETE':
         market = Market.objects.get(pk=pk)
         serializer = MarketSerializer(market)
         market.delete()
         return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def sellers_view(request):
+
+    if request.method == 'GET':
+        sellers = Seller.objects.all()
+        serializer = SellerDetailSerializer(sellers, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = SellerCreateSerializer(
+            data=request.data,
+            partial=True
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
